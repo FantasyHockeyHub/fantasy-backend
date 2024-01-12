a2d6ee7990b454768e217f7aa812568c2278cb7f
Daniil <dan.ponyavin@yandex.ru>
2024-01-12 16:59:18 +0300

    Registration template created

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Frozen-Fantasy/fantasy-backend.git/config"
 	"github.com/Frozen-Fantasy/fantasy-backend.git/pkg/api"
+	"github.com/Frozen-Fantasy/fantasy-backend.git/pkg/service/user"
 	"github.com/Frozen-Fantasy/fantasy-backend.git/pkg/storage"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -12,11 +13,15 @@ import (
 func Core() fx.Option {
 	return fx.Options(
 		fx.Provide(
+			fx.Annotate(storage.NewPostgresStorage, fx.As(new(user.Storage))),
+		),
+		fx.Provide(
 			context.Background,
 			storage.NewPostgresStorage,
 			config.NewConfig,
 			gin.Default,
 			api.NewApi,
+			user.NewService,
 		),
 		fx.Invoke(restAPIHook),
 	)
---
+++ pkg/storage/user.go
@@ -0,0 +1,8 @@
+package storage
+
+import "context"
+
+func (p *PostgresStorage) CreateUser(ctx context.Context) error {
+
+	return nil
+}
